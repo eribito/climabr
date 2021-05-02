2840bc86b52350322464cf4d066f2bf97ed4488b
eribito <57811230+eribito@users.noreply.github.com>
2021-05-02 13:11:44 -0300

    Add query history

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,25 +2,35 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { City } from 'src/domain/entities/city';
 import { SearchCityService } from 'src/domain/services/search-city.service';
+import { LocalStorageCitiesService } from '../../domain/services/local-storage-cities.service';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements ViewWillEnter {
   cities: City[];
   hasError: boolean = false;
+  hasSavedCitiesOnHistory: boolean = false;
   errorMessage: string;
 
   constructor(
     private readonly searchService: SearchCityService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly storageCities: LocalStorageCitiesService,
   ) {}
+
+  async ionViewWillEnter() {
+    this.cities = await this.storageCities.getCities();
+    this.hasSavedCitiesOnHistory = this.cities.length > 0;
+  }
 
   async onSearch(query: string) {
     try {
       this.hasError = false;
+      this.hasSavedCitiesOnHistory = false;
       this.cities = await this.searchService.search(query);
     } catch (error) {
       this.hasError = true;
@@ -29,6 +39,7 @@ export class HomePage {
   }
 
   onSelectCity(cityId: string) {
+    this.storageCities.addCityToHistory(this.cities.find(c => c.id === parseInt(cityId, 10)));
     this.router.navigateByUrl(`/weather/${cityId}`);
   }
 }
